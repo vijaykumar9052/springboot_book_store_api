a8bb9512a74b385cf97bda4dc50c99df2e79dcf8
Your Name <you@example.com>
2024-11-15 12:31:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# springboot_book_store_api
+# springboot_book_store_api
+# springboot_book_store_api
---
+++ src/main/java/in/ashokit/repo/BookRepo.java
@@ -0,0 +1,7 @@
+package in.ashokit.repo;
+
+import in.ashokit.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepo extends JpaRepository<Book, Integer> {
+}
