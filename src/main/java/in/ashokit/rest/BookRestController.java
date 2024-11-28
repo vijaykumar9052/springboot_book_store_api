0d7a751b9c92a19fda2401f532e07ad55210f34c
vijay <vijaykumarche68@gmail.com>
2024-11-28 13:09:08 +0530

    remove return

--- src/main/java/in/ashokit/rest/BookRestController.java
+++ src/main/java/in/ashokit/rest/BookRestController.java
@@ -56,7 +56,9 @@ public class BookRestController {
             return new ResponseEntity<>(byId.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.OK);
-        return new ResponseEntity<>(null, HttpStatus.OK);
+       
+
+
 
     }
 }
