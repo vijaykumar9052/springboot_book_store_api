a24344dd4aeff3db2e9ccdcede52067e1a76de71
vijay <vijaykumarche68@gmail.com>
2025-01-02 16:21:14 +0530

    repo commit

--- src/main/java/in/ashokit/repo/BookRepo.java
+++ src/main/java/in/ashokit/repo/BookRepo.java
@@ -4,4 +4,5 @@ import in.ashokit.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepo extends JpaRepository<Book, Integer> {
+//=================================================================
 }
--- src/main/java/in/ashokit/rest/BookRestController.java
+++ src/main/java/in/ashokit/rest/BookRestController.java
@@ -1,9 +1,7 @@
 package in.ashokit.rest;
 
-
 import in.ashokit.entity.Book;
 import in.ashokit.repo.BookRepo;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,50 +13,47 @@ import java.util.Optional;
 @RestController
 public class BookRestController {
 
-    @Autowired
-    private BookRepo bookRepo;
+	@Autowired
+	private BookRepo bookRepo;
 
-    @PostMapping("/book")
-    public ResponseEntity<Book> saveBook(@RequestBody Book book){
-        Book save = bookRepo.save(book);
-        return new ResponseEntity<>(save, HttpStatus.CREATED);
-    }
+	@PostMapping("/book")
+	public ResponseEntity<Book> saveBook(@RequestBody Book book) {
+		Book save = bookRepo.save(book);
+		return new ResponseEntity<>(save, HttpStatus.CREATED);
+	}
 
-    @PutMapping("/book/{bookId}")
-    public ResponseEntity<Book> saveBook(@RequestBody Book book, @PathVariable Integer bookId){
-        book.setBookId(bookId);
-        Book save = bookRepo.save(book);
-        return new ResponseEntity<>(save, HttpStatus.OK);
-    }
+	@PutMapping("/book/{bookId}")
+	public ResponseEntity<Book> saveBook(@RequestBody Book book, @PathVariable Integer bookId) {
+		book.setBookId(bookId);
+		Book save = bookRepo.save(book);
+		return new ResponseEntity<>(save, HttpStatus.OK);
+	}
 
-    @GetMapping("/books")
-    public ResponseEntity<List<Book>> getBooks(){
-        List<Book> all = bookRepo.findAll();
-        return new ResponseEntity<>(all, HttpStatus.OK);
-    }
+	@GetMapping("/books")
+	public ResponseEntity<List<Book>> getBooks() {
+		List<Book> all = bookRepo.findAll();
+		return new ResponseEntity<>(all, HttpStatus.OK);
+	}
 
-    @GetMapping("/book/{bookId}")
-    public ResponseEntity<Book> getBook(@PathVariable Integer bookId){
-        Optional<Book> byId = bookRepo.findById(bookId);
-        if(byId.isPresent()){
-            return new ResponseEntity<>(byId.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.OK);
+	@GetMapping("/book/{bookId}")
+	public ResponseEntity<Book> getBook(@PathVariable Integer bookId) {
+		Optional<Book> byId = bookRepo.findById(bookId);
+		if (byId.isPresent()) {
+			return new ResponseEntity<>(byId.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.OK);
 
-    }
+	}
 
-    @DeleteMapping("/book/{bookId}")
-    public ResponseEntity<Book> deleteBook(@PathVariable Integer bookId){
-        Optional<Book> byId = bookRepo.findById(bookId);
+	@DeleteMapping("/book/{bookId}")
+	public ResponseEntity<Book> deleteBook(@PathVariable Integer bookId) {
+		Optional<Book> byId = bookRepo.findById(bookId);
 
-        if(byId.isPresent()){
-            bookRepo.deleteById(bookId);
-            return new ResponseEntity<>(byId.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.OK);
-       
+		if (byId.isPresent()) {
+			bookRepo.deleteById(bookId);
+			return new ResponseEntity<>(byId.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.OK);
 
-
-
-    }
+	}
 }
